8191284164f03de30e4bca959f6a4d59d439983b
pai-plznw4me <plznw4me@publicai.co.kr>
2022-01-22 13:14:15 +0900

    generic view 업데이트

--- lecture_model/mysite/polls/urls.py
+++ lecture_model/mysite/polls/urls.py
@@ -5,6 +5,6 @@ app_name = 'polls'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('<int:pk>/', DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', ResultsView.as_view()),
+    path('<int:pk>/results/', ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', vote, name='vote'),
 ]
